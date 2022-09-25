f1029dcaec810d41220ed2dc9fa65b5a48bf373d
TheCheese-BloodPrince <karasiaarav@gmail.com>
2022-09-24 21:58:47 -0700

    #4 IS COMPLETE :DDDDDD

--- Four/Main.java
+++ Four/Main.java
@@ -1,37 +1,41 @@
+
 // we import this to access the Scanner class
 import java.util.Scanner;
 
 class Main {
 
-// This is the main method where we can call functions
+  // This is the main method where we can call functions
   public static void main(String[] args) {
     Scanner n = new Scanner(System.in);
     System.out.println("What would you like the length of the board to be? (3-9)");
     int size = n.nextInt();
-    if (!(size >= 3 && size <= 9)) {
+
+    if (!(size > 3 && size <= 9)) {
       System.out.println("Invalid size");
       System.exit(0);
     }
-
-
+    
     long startTime = System.currentTimeMillis();
     System.out.println(System.currentTimeMillis());
 
-
     // n is a Scanner object
-    
 
     System.out.println(
         "Welcome to Battleship! Your objective is to sink all of the enemy's ships."
-         + "There's two 3-spaced ships hidden below the surface."
-    );
+            + "There's two 3-spaced ships hidden below the surface.");
     System.out.println();
-
     // Here we create a grid of 10 by 10 where each tile is a bool
     int[][] gridLayout = new int[size][size];
-    for (int i = 0; i<gridLayout.length; i++) {
-      for (int j = 0; j<gridLayout[i].length; j++) {
+    for (int i = 0; i < gridLayout.length; i++) {
+      for (int j = 0; j < gridLayout[i].length; j++) {
         gridLayout[i][j] = 0;
+      }
+    }
+
+    int[][] aigridLayout = new int[size][size];
+    for (int i = 0; i < aigridLayout.length; i++) {
+      for (int j = 0; j < aigridLayout[i].length; j++) {
+        aigridLayout[i][j] = 0;
       }
     }
 
@@ -42,30 +46,40 @@ class Main {
 
     double numberOfTry = 0.0;
     double numberOfHits = 0.0;
+    int ainumberOfHits = 0;
     // number of ships on the board
     int numberOfSquares = 6;
+    int ainumberOfSquares = 6;
 
     // Create the grid where the ships can be placed (same as the gridLayout)
     boolean[][] shipArray = new boolean[size][size];
+    boolean[][] aiArray = new boolean[size][size];
 
     // One random position on the grid
     // Start point of the ships
-    int randStartRow = (int) (Math.random() * (size-1));
-    int randStartCol = (int) (Math.random() * (size-1));
-    int storeShip1Row = (int) (Math.random() * (size-1));
-    int storeShip1Col = (int) (Math.random() * (size-1));
+    int randStartRow = (int) (Math.random() * (size - 1));
+    int randStartCol = (int) (Math.random() * (size - 1));
+    int storeShip1Row = (int) (Math.random() * (size - 1));
+    int storeShip1Col = (int) (Math.random() * (size - 1));
+
+    int airandStartRow = (int) (Math.random() * (size - 1));
+    int airandStartCol = (int) (Math.random() * (size - 1));
+    int aistoreShip1Row = (int) (Math.random() * (size - 1));
+    int aistoreShip1Col = (int) (Math.random() * (size - 1));
 
     // Set the tile that storeShip1 and storShip2 are on to true
     shipArray[storeShip1Row][randStartCol] = true;
     shipArray[randStartRow][storeShip1Col] = true;
+    aiArray[aistoreShip1Row][airandStartCol] = true;
+    aiArray[airandStartRow][aistoreShip1Col] = true;
 
     // Check if the storeShip1Row is on on row 9: If it is then add another ship
     // that is 1 to the left and set that tile to true ; if not then add a ship 1 to
     // the right and set that tile to true (Note: the storeShip1Row is 2 tiles long)
-    if (storeShip1Row + 3 <= (size-1)) {
+    if (storeShip1Row + 3 <= (size - 1)) {
       shipArray[storeShip1Row][randStartCol + 1] = true;
       shipArray[storeShip1Row][randStartCol + 2] = true;
-    } else if (storeShip1Col - 3 <= 0){
+    } else if (storeShip1Col - 3 <= 0) {
       shipArray[storeShip1Row][randStartCol - 1] = true;
       shipArray[storeShip1Row][randStartCol - 2] = true;
 
@@ -73,18 +87,34 @@ class Main {
       numberOfSquares -= 3;
     }
 
+    if (aistoreShip1Row + 3 <= (size - 1)) {
+      aiArray[aistoreShip1Row][airandStartCol + 1] = true;
+      aiArray[aistoreShip1Row][airandStartCol + 2] = true;
+    } else if (aistoreShip1Col - 3 <= 0) {
+      aiArray[aistoreShip1Row][airandStartCol - 1] = true;
+      aiArray[aistoreShip1Row][airandStartCol - 2] = true;
+
+    } else {
+      ainumberOfSquares -= 3;
+    }
+
     // Check if the storeShip1Col is on the 9th tile: if it is not then add 3 ships
     // down and set all to true else add 3 ships up and set them to true
-    if (storeShip1Col + 3 <= (size-1)) {
+    if (storeShip1Col + 3 <= (size - 1)) {
       shipArray[randStartRow][storeShip1Col + 1] = true;
       shipArray[randStartRow][storeShip1Col + 2] = true;
     } else if (storeShip1Col - 3 <= 0) {
       shipArray[randStartRow][storeShip1Col - 1] = true;
       shipArray[randStartRow][storeShip1Col - 2] = true;
-    } else {
-      numberOfSquares -= 3;
     }
-    
+
+    if (aistoreShip1Col + 3 <= (size - 1)) {
+      aiArray[airandStartRow][aistoreShip1Col + 1] = true;
+      aiArray[airandStartRow][aistoreShip1Col + 2] = true;
+    } else if (aistoreShip1Col - 3 <= 0) {
+      aiArray[airandStartRow][aistoreShip1Col - 1] = true;
+      aiArray[airandStartRow][aistoreShip1Col - 2] = true;
+    }
 
     /*
      * Check if the user hits all of the ships equal to the numberOfSquares
@@ -100,14 +130,14 @@ class Main {
       int columnInput = n.nextInt();
 
       // Checks if the user inputs are in bound from 0 to 9
-      if (rowInput > (size-1) || rowInput < 0 || columnInput > (size-1) || columnInput < 0) {
+      if (rowInput > (size - 1) || rowInput < 0 || columnInput > (size - 1) || columnInput < 0) {
         System.out.println("Input not allowed in grid. Please try again");
         System.out.print("Pick a row: ");
         rowInput = n.nextInt();
         System.out.print("Pick a column: ");
         columnInput = n.nextInt();
       }
-      
+
       // Checks if the rowInput and columInput hit a ship on the shipArray grid and
       // not the gridLayout
       if (shipArray[rowInput][columnInput]) {
@@ -128,18 +158,44 @@ class Main {
         layout(gridLayout, size);
         // Always increase numberOfTry everytime you miss
         numberOfTry++;
-
-        gridLayout[rowInput][columnInput] = 1;
       }
 
       // check if we won (Hint: this is the final part of the program)
       if (numberOfHits == numberOfSquares) {
-        
+
         // breaks the loops
         break;
       }
+
+      int airowInput = (int) (Math.random() * (size - 1));
+      int aicolumnInput = (int) (Math.random() * (size - 1));
+
+      if (aiArray[airowInput][aicolumnInput]) {
+        ainumberOfHits++;
+        aigridLayout[airowInput][aicolumnInput] = 2;
+        System.out.println("AI TURN:");
+        layout(aigridLayout, size);
+      }
+
+      if (!aiArray[airowInput][aicolumnInput]) {
+        aigridLayout[airowInput][aicolumnInput] = 1;
+        System.out.println("AI TURN:");
+        layout(aigridLayout, size);
+      }
+      if (ainumberOfHits == ainumberOfSquares) {
+        aiWin();
+      }
+
     }
-    System.out.println("Congratulations! Your hit percentage was: " + (100*((numberOfHits/(numberOfHits+numberOfTry)))) + "%!\nThe total guesses you took were: " + (numberOfHits+numberOfTry) + "!\nYour overall time was: " + (System.currentTimeMillis()-startTime)/60000 + "minutes!");
+    System.out
+        .println("YOU WON!\n Your hit percentage was: " + (100 * ((numberOfHits / (numberOfHits + numberOfTry))))
+            + "%!\nThe total guesses you took were: " + (numberOfHits + numberOfTry) + "!\nYour overall time was: "
+            + (System.currentTimeMillis() - startTime) / 60000 + " minutes!");
+  }
+
+  public static void aiWin() {
+    System.out.println("AI WON!");
+    System.exit(0);
   }
 
   // function that creates the board evertime until you win
@@ -147,7 +203,7 @@ class Main {
     int storeNumOut = 0;
     // prints the row and column numbers
     String e = "  ";
-    for (int i = 0; i<size; i++) {
+    for (int i = 0; i < size; i++) {
       e += Integer.toString(i);
       e += "  ";
     }
@@ -172,7 +228,7 @@ class Main {
         if (gridLayout[r][c] == 0) {
           System.out.print(" - ");
         } else if (gridLayout[r][c] == 1) {
-          System.out.print("\u001B[31m"+" O "+"\u001B[37m");
+          System.out.print("\u001B[31m" + " O " + "\u001B[37m");
         } else if (gridLayout[r][c] == 2) {
           System.out.print("\u001B[32m" + " X " + "\u001B[37m");
         }
